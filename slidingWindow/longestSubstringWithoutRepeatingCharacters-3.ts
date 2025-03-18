c0a2d8eef31711f6f6e2a5acc798252a8f1e0449
Shawn Snyder <shawnsnyder@shawns-mac-mini.lan>
2025-03-18 10:37:57 -0400

    longsubstring

---
+++ slidingWindow/longestSubstringWithoutRepeatingCharacters-3.ts
@@ -0,0 +1,20 @@
+function lengthOfLongestSubstring(s: string): number {
+  let maxLength = 0;
+  const charSet = new Set();
+  let l = 0;
+  let res = 0;
+
+  for (let r = 0; r < s.length; r++) {
+      while(charSet.has(s[r])){
+         charSet.delete(s[l])
+         l++ 
+      }
+
+      charSet.add(s[r])
+      res = Math.max(res, r - l + 1)
+  }
+  return res;
+}
+
+//O(n) 
+//O(m) //m is the amount of unique characters in string
